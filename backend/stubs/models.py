25821c21b93d704edd7ea6d4bfe50fbef90c24ea
Emma Randolph <emmalrandolph@gmail.com>
2023-04-04 15:04:06 -0500

    Initial backend commit

--- backend/stubs/models.py
+++ backend/stubs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Bank(models.Model):
+    cardNum = models.FloatField
+
+    def float (self):
+        return self.cardNum
+
+        
